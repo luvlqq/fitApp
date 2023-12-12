fa928c8f0c9912657c345edda387906e0d4eecc7
luvlqq <lualgod92@gmail.com>
2023-12-12 19:01:23 +0300

    test: create test for exercises

---
+++ apps/workouts/src/modules/exercises/tests/exercise.service.spec.ts
@@ -0,0 +1,54 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ExerciseRepository } from '../exercise.repository';
+import { ExerciseMicroserviceService } from '../exercise.service';
+import { PrismaModule } from '@app/db';
+import { CreateExerciseDto } from '../dto/craete.exercise.dto';
+import { groupOfMusculesENUM } from '@prisma/client';
+
+describe('ExerciseService', () => {
+  let service: ExerciseMicroserviceService;
+  let repository: ExerciseRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ExerciseMicroserviceService, ExerciseRepository],
+      imports: [PrismaModule],
+    }).compile();
+
+    service = module.get<ExerciseMicroserviceService>(
+      ExerciseMicroserviceService,
+    );
+    repository = module.get<ExerciseRepository>(ExerciseRepository);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('Create exercise', () => {
+    it('should call repository to create a correct exercise', async () => {
+      const dto: CreateExerciseDto = {
+        name: 'Test Exercise',
+        description: 'Test Description',
+        video: 'Test Video',
+        gropuOfMuscules: groupOfMusculesENUM.Back,
+      };
+
+      const expectedExercise = {
+        id: 1,
+        name: 'Test Exercise',
+        description: 'Test Description',
+        video: 'Test Video',
+        workoutId: 1,
+        gropuOfMuscules: groupOfMusculesENUM.Back,
+      };
+
+      jest
+        .spyOn(repository, 'createExercise')
+        .mockResolvedValue(expectedExercise);
+
+      const createdExercise = await service.createExercise(dto);
+      expect(createdExercise).toEqual(expectedExercise);
+    });
+  });
+});
